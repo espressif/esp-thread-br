d4a49ac22aa1c7a5f5ce7a9102f242114be0055c
Jiacheng Guo <guoamtcmp@gmail.com>
2022-07-15 17:50:50 +0800

    ci: add clang-format check
    The MR formats all the code with clang-format and forces the format in
    the gitlab CI.

--- components/esp_ot_cli_extension/include/esp_ot_udp_socket.h
+++ components/esp_ot_cli_extension/include/esp_ot_udp_socket.h
@@ -10,7 +10,7 @@
  * Unless required by applicable law or agreed to in writing, this
  * software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
  * CONDITIONS OF ANY KIND, either express or implied.
-*/
+ */
 
 #pragma once
 
