8a59071b3eabf17832e821e2eac874ee77f62bdb
Chen Yu Dong <chenyudong@espressif.com>
2024-03-22 15:03:24 +0800

    update: use iperf from component registry

--- tools/ci/ot_cli.cmake
+++ tools/ci/ot_cli.cmake
@@ -8,7 +8,5 @@ set(ENV{EXTRA_CFLAGS} "-Werror -Werror=deprecated-declarations -Werror=unused-va
 set(ENV{EXTRA_CXXFLAGS} "-Werror -Werror=deprecated-declarations -Werror=unused-variable \
     -Werror=unused-but-set-variable -Werror=unused-function")
 
-set(EXTRA_COMPONENT_DIRS $ENV{IDF_PATH}/examples/common_components/iperf)
-
 include($ENV{IDF_PATH}/tools/cmake/project.cmake)
 project(esp_ot_cli)
