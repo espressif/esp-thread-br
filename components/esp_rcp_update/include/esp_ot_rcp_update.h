4c1b767406eca764b4f4c88bfbdd21df3d9b2b13
Xu Si Yu <xusiyu@espressif.com>
2025-11-25 15:16:39 +0800

    feat(rcp_update): add new APIs for openthread rcp update

---
+++ components/esp_rcp_update/include/esp_ot_rcp_update.h
@@ -0,0 +1,58 @@
+/*
+ * SPDX-FileCopyrightText: 2025 Espressif Systems (Shanghai) CO LTD
+ *
+ * SPDX-License-Identifier: CC0-1.0
+ *
+ * OpenThread Command Line Example
+ *
+ * This example code is in the Public Domain (or CC0 licensed, at your option.)
+ *
+ * Unless required by applicable law or agreed to in writing, this
+ * software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+ * CONDITIONS OF ANY KIND, either express or implied.
+ */
+
+#ifndef ESP_OT_RCP_UPDATE_H
+#define ESP_OT_RCP_UPDATE_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * @brief Attempts to update the RCP.
+ *
+ * @note If @p running_rcp_version is not NULL, the stored version will be
+ *       compared with the running version to determine whether an update
+ *       is required. If it is NULL, the stored version will be used to
+ *       force an update.
+ *
+ * @note This function must be called after esp_rcp_update_init.
+ *       This function may call esp_restart() if an update is performed.
+ *
+ * @param[in] running_rcp_version  The currently running RCP version.
+ */
+void esp_ot_try_update_rcp(const char *running_rcp_version);
+
+/**
+ * @brief Registers the handlers used to process RCP-related errors.
+ *
+ * @note The registered handlers rely on esp_rcp_update_init, and therefore
+ *       esp_rcp_update_init must be called before any handler is triggered.
+ *       However, the registration itself does not require a specific call order.
+ */
+void esp_ot_register_rcp_handler(void);
+
+/**
+ * @brief Triggers an RCP update if the running RCP version does not match
+ *        the version stored in flash.
+ *
+ * @note This function must be called after esp_rcp_update_init.
+ */
+void esp_ot_update_rcp_if_different(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // ESP_OT_RCP_UPDATE_H
---
+++ components/esp_rcp_update/src/esp_ot_rcp_update.c
@@ -0,0 +1,79 @@
+/*
+ * SPDX-FileCopyrightText: 2025 Espressif Systems (Shanghai) CO LTD
+ *
+ * SPDX-License-Identifier: CC0-1.0
+ *
+ * OpenThread Command Line Example
+ *
+ * This example code is in the Public Domain (or CC0 licensed, at your option.)
+ *
+ * Unless required by applicable law or agreed to in writing, this
+ * software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+ * CONDITIONS OF ANY KIND, either express or implied.
+ */
+#include "esp_ot_rcp_update.h"
+#include "esp_log.h"
+#include "esp_openthread.h"
+#include "esp_openthread_lock.h"
+#include "esp_openthread_spinel.h"
+#include "esp_rcp_update.h"
+#include "openthread/platform/radio.h"
+
+#define TAG "ot_rcp_update"
+#define ESP_RCP_VERSION_MAX_SIZE 100
+
+void esp_ot_try_update_rcp(const char *running_rcp_version)
+{
+    char internal_rcp_version[ESP_RCP_VERSION_MAX_SIZE];
+    if (running_rcp_version) {
+        ESP_LOGI(TAG, "Running  RCP Version: %s", running_rcp_version);
+    }
+    if (esp_rcp_load_version_in_storage(internal_rcp_version, sizeof(internal_rcp_version)) == ESP_OK) {
+        ESP_LOGI(TAG, "Internal RCP Version: %s", internal_rcp_version);
+        if (running_rcp_version && strcmp(internal_rcp_version, running_rcp_version) == 0) {
+            esp_rcp_mark_image_verified(true);
+        } else {
+            esp_openthread_rcp_deinit();
+            if (esp_rcp_update() == ESP_OK) {
+                esp_rcp_mark_image_verified(true);
+            } else {
+                esp_rcp_mark_image_verified(false);
+            }
+            esp_restart();
+        }
+    } else {
+        ESP_LOGI(TAG, "RCP firmware not found in storage");
+        esp_rcp_mark_image_verified(false);
+        esp_restart();
+    }
+}
+
+static void rcp_hardware_reset_handler(void)
+{
+    esp_rcp_reset();
+}
+
+static void rcp_compatibility_error_handler(void)
+{
+    esp_ot_try_update_rcp(NULL);
+}
+
+static void rcp_failure_handler(void)
+{
+    esp_ot_try_update_rcp(NULL);
+}
+
+void esp_ot_register_rcp_handler(void)
+{
+    esp_openthread_register_rcp_failure_handler(rcp_hardware_reset_handler);
+    esp_openthread_set_compatibility_error_callback(rcp_compatibility_error_handler);
+    esp_openthread_set_coprocessor_reset_failure_callback(rcp_failure_handler);
+}
+
+void esp_ot_update_rcp_if_different(void)
+{
+    esp_openthread_lock_acquire(portMAX_DELAY);
+    const char *running_rcp_version = otPlatRadioGetVersionString(esp_openthread_get_instance());
+    esp_openthread_lock_release();
+    esp_ot_try_update_rcp(running_rcp_version);
+}
